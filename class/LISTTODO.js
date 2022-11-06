433b01163e4b8c2cf0a0f70b052c32349ae02d24
Benjamin D <112513857+deZ1711@users.noreply.github.com>
2022-11-06 23:32:53 +0100

    pb addtask
    probleme sur l'ajout de tache

--- class/LISTTODO.js
+++ class/LISTTODO.js
@@ -6,4 +6,46 @@ export class LISTTODO {
     this.#liste = liste
   }
 
+  appendToDo(element) {
+    element.innerHTML = `<ul class="taches-list">
+    </ul>`
+    const ul = element.querySelector('.taches-list')
+    for (let tache of this.#liste) {
+      const t = new TODO(tache)
+      t.createToDo(ul)
+    }
+  }
+}
+
+class TODO {
+
+  #tache
+
+  constructor (todo) {
+    const li = document.createElement('li')
+    li.classList.add(`tache-${todo.nbr}`)
+
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.setAttribute('id', `tache-${todo.nbr}`)
+
+    
+    const label = document.createElement('label')
+    label.setAttribute('for', `tache-${todo.nbr}`)
+    label.innerText = todo.title
+    
+    const trashBtn = document.createElement('button')
+    trashBtn.classList.add('trash')
+    trashBtn.innerHTML = `<svg><path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z"/><path fill-rule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.133 2.845a.75.75 0 011.06 0l1.72 1.72 1.72-1.72a.75.75 0 111.06 1.06l-1.72 1.72 1.72 1.72a.75.75 0 11-1.06 1.06L12 15.685l-1.72 1.72a.75.75 0 11-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06z" clip-rule="evenodd"/></svg>`
+    
+    li.append(checkbox)
+    li.append(label)
+    li.append(trashBtn)
+
+    this.#tache = li
+  }
+
+  createToDo(element) {
+    element.append(this.#tache)
+  }
 }
